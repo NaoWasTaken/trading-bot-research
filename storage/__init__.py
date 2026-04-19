4768dddf406a5057234a83998a729b77b636550e
Aviel <aviboyd04@gmail.com>
2026-04-19 18:08:16 -0400

    Update storage package exports

--- storage/__init__.py
+++ storage/__init__.py
@@ -1,3 +1,10 @@
 from .db import DecisionLog, RecentDecisionLog, TradingLogStore
+from .forward_log import ForwardLogResult, append_forward_test_log
 
-__all__ = ["DecisionLog", "RecentDecisionLog", "TradingLogStore"]
+__all__ = [
+    "DecisionLog",
+    "ForwardLogResult",
+    "RecentDecisionLog",
+    "TradingLogStore",
+    "append_forward_test_log",
+]
